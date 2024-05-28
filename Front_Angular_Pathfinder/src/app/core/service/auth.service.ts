cb7d4b80128a8be4574b432777f34dbc3a79bcc3
dyfmeks <j.jordan.quispe.supo@gmail.com>
2024-05-28 12:09:21 -0500

    newFrontend

--- Front_Angular_Pathfinder/src/app/core/service/auth.service.ts
+++ Front_Angular_Pathfinder/src/app/core/service/auth.service.ts
@@ -1,22 +1,49 @@
-import {Injectable} from '@angular/core';
-import {Observable, of} from 'rxjs';
-import {delay, tap} from 'rxjs/operators';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { User } from '../models/user';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-   providedIn: 'root',
+  providedIn: 'root',
 })
 export class AuthService {
-   isLoggedIn = false;
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
 
-   login(): Observable<boolean> {
-      return of(true)
-         .pipe(
-            delay(1000),
-            tap(() => this.isLoggedIn = true)
-         );
-   }
+  constructor(private http: HttpClient) {
+    this.currentUserSubject = new BehaviorSubject<User>(
+      JSON.parse(localStorage.getItem('currentUser'))
+    );
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
 
-   logout(): void{
-      this.isLoggedIn = false;
-   }
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
+
+  login(username: string, password: string) {
+    return this.http
+      .post<any>(`${environment.apiUrl}/authenticate`, {
+        username,
+        password,
+      })
+      .pipe(
+        map((user) => {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+          return user;
+        })
+      );
+  }
+
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('currentUser');
+    this.currentUserSubject.next(null);
+    return of({ success: false });
+  }
 }
