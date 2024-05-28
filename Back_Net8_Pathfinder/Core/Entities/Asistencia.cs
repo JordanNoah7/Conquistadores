cddd91de993eb9a8bfc306a8f690ec6727c975f8
dyfmeks <j.jordan.quispe.supo@gmail.com>
2024-05-28 09:39:44 -0500

    parametersAndSessions

--- Back_Net8_Pathfinder/Core/Entities/Asistencia.cs
+++ Back_Net8_Pathfinder/Core/Entities/Asistencia.cs
@@ -14,25 +14,25 @@ public class Asistencia
     public DateTime AsisFecha { get; set; }
     [Required(ErrorMessage = "La frecuencia es obligatoria")]
     [Column(TypeName = "tinyint")]
-    public int AsisFrecuencia { get; set; }
+    public byte AsisFrecuencia { get; set; }
     [Required(ErrorMessage = "La devoción matutina es obligatoria")]
     [Column(TypeName = "tinyint")]
-    public int AsisDevocion { get; set; }
+    public byte AsisDevocion { get; set; }
     [Required(ErrorMessage = "La cuota es obligatoria")]
     [Column(TypeName = "tinyint")]
-    public int AsisCuota { get; set; }
+    public byte AsisCuota { get; set; }
     [Required(ErrorMessage = "La disciplina es obligatoria")]
     [Column(TypeName = "tinyint")]
-    public int AsisDisciplina { get; set; }
+    public byte AsisDisciplina { get; set; }
     [Required(ErrorMessage = "El año bíblico es obligatorio")]
     [Column(TypeName = "tinyint")]
-    public int AsisAnioBiblico { get; set; }
+    public byte AsisAnioBiblico { get; set; }
     [Required(ErrorMessage = "Los requerimientos son obligatorios")]
     [Column(TypeName = "tinyint")]
-    public int AsisRequerimiento { get; set; }
+    public byte AsisRequerimiento { get; set; }
     [Required]
     [Column(TypeName = "tinyint")]
-    public int AsisTotal { get; set; }
+    public byte AsisTotal { get; set; }
     [Required(ErrorMessage = "El monto es obligatorio")]
     [Column(TypeName = "decimal(9,3)")]
     public float AsisMonto { get; set; }
---
+++ Back_Net8_Pathfinder/Core/Entities/Parametros.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Core.Entities;
+
+public class Parametros
+{
+    [Key]
+    public int ParaID { get; set; }
+    [Required(ErrorMessage = "El nombre del parámetro es obligatorio")]
+    [Column(TypeName = "nvarchar(25)")]
+    public string ParaNombre { get; set; }
+    [Required(ErrorMessage = "El valor del parámetro es obligatorio")]
+    [Column(TypeName = "nvarchar(250)")]
+    public string ParaValor { get; set; }
+    
+    [Required]
+    [Column(TypeName = "nvarchar(20)")]
+    public string AudiUserCrea { get; set; }
+    [Required]
+    [Column(TypeName = "datetime")]
+    public DateTime AudiFechCrea { get; set; }
+    [Required]
+    [Column(TypeName = "nvarchar(20)")]
+    public string AudiHostCrea { get; set; }
+    [Column(TypeName = "nvarchar(20)")]
+    public string? AudiUserMod { get; set; }
+    [Column(TypeName = "datetime")]
+    public DateTime? AudiFechMod { get; set; }
+    [Column(TypeName = "nvarchar(20)")]
+    public string? AudiHostMod { get; set; }
+}
