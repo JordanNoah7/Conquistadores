933c52373344e111477ed255c42b5b257864a09d
dyfmeks <j.jordan.quispe.supo@gmail.com>
2024-06-30 00:13:37 -0500

    db

--- DB_SQLServer_Pathfinder/prueba.sql
+++ DB_SQLServer_Pathfinder/prueba.sql
@@ -4,19 +4,33 @@
 
 USE DB_SqlServer_Pathfinder
 GO
-SELECT * FROM Conquistadores
-SELECT * FROM Usuarios --6b043205253a236a414f0ac9aaff1338
-select * from ActividadConquistadores
-select * from Roles
-select * from RolesUsuario
-select * from Tutores
+SELECT *
+FROM Conquistadores
+SELECT *
+FROM Clases
+SELECT *
+FROM Unidades
+SELECT *
+FROM Usuarios
+--6b043205253a236a414f0ac9aaff1338
+select *
+from ActividadConquistadores
+select *
+from Roles
+select *
+from RolesUsuario
+select *
+from Tutores
 
-select * from parametros
-SELECT * FROM Sesiones
+select *
+from parametros
+SELECT *
+FROM Sesiones
 
-select * from Asistencias
-select * from Inscripciones
+select *
+from Asistencias
+select *
+from Inscripciones
 
-update Usuarios
-set UsuaCambiarContrasenia = 1
-where UsuaId = 1
+SELECT *
+FROM ConquistadorEspecialidades
